65203e2727fb8094453cc2e514f957910ba60536
aijoshgx-maker <aijoshgx-maker@users.noreply.github.com>
2026-08-18 14:17:54 +0000

    deploy: 209af1d4796d322574ad7832407d460464bd0173

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2008553597" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "26948295" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
